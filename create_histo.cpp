e1f47fb47f69e86d1764f1aac7aa3b6ccc6b1488
Matteo Malucchi <matteomalucchi13@gmail.com>
2022-05-12 16:04:46 +0200

    create tree and histo

--- create_histo.cpp
+++ create_histo.cpp
@@ -1,86 +1,67 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
+
 using namespace std;
 
 
-vector<vector<double>> read_time(string name, int numb){
-    ifstream myfile;
-    myfile.open("data/"+name+".dat", ios::in | ios::out);  
-    vector<vector<double>> v (7);
-    vector<vector<double>> w (numb);    
+TH1D* fill_histo(TFile* tree_file, string name, vector<double> ranges){
+    double t;
+    TTree* tree = (TTree*) tree_file->Get(&(name)[0]);
+    TH1D *histo = new TH1D(&(name)[0],&(name)[0], ranges[0], ranges[1], ranges[2]);
 
-    if (myfile.is_open()){
-        string tp;
-        while(getline(myfile, tp)){ //read data from file object and put it into string.
-            int ch_numb = stoi(tp.substr(0));
-            //cout <<ch_numb << tp.substr(8, tp.length()-8)<<endl;
-            v[ch_numb-1].push_back(stod(tp.substr(2, tp.length()-1))); 
-            //cout << v[0][1] <<endl;
-            if (ch_numb == 2 || ch_numb == 3){
-                w[ch_numb-2].push_back(v[ch_numb-1][v[ch_numb-1].size()-1]-v[0][v[0].size()-1]);
-                cout << setprecision(8)<<w[1][w[1].size()-1]<<endl;
-
-            }
-        }
-        myfile.close(); //close the file object.
-    }    
-    return w;
-} 
-
-vector<TH1D*> fill_histo(vector<vector<double>> diff_time, string name, vector<double> range){
-    vector<TH1D*> histo;
-    string name_tot = name+"exp_tot";
-    TH1D* h_tot= new TH1D(&(name_tot)[0], &(name_tot)[0], range[0], range[1], range[2]);
-    
-    for (int i=0; i<diff_time.size(); i++){
-        string name_ch = name+"_exp_channel"+to_string(i+1);
-        TH1D* h= new TH1D(&(name_ch)[0], &(name_ch)[0], range[0], range[1], range[2]);        
-        for (int j=0; j<diff_time[i].size(); j++){
-            h->Fill(diff_time[i][j]);
-            h_tot->Fill(diff_time[i][j]);
-        }
-        histo.push_back(h);
+    Int_t nentries = (Int_t)tree->GetEntries();
+    cout << nentries << endl;
+    tree->SetBranchAddress("t",&t);
+    for (Int_t i=0; i<nentries; i++) {
+        tree->GetEntry(i);
+        histo->Fill(t);
     }
-    histo.push_back(h_tot);
+    histo->GetXaxis()->SetTitle("time [#mu s]");
+    histo->GetYaxis()->SetTitle(&("Entries / "+to_string(histo->GetBinWidth(1)))[0]);
     return histo;
 }
 
-void fit_exp(TH1D* hist, vector<double>range, vector<double> exp_param){
-    string name =hist->GetName();
+void fit_exp(TH1D* histo, vector<double>range, vector<double> exp_param){
+    string name =histo->GetName();
     TCanvas *c = new TCanvas(&(name)[0], &(name)[0]);
     TF1 *f = new TF1("f", "[0]*exp(-x/[1])", range[1], range[2]);
     f->SetParNames("norm", "tau"); 
     f->SetParameters(exp_param[0], exp_param[1]);
-    hist->Fit("f");
-    hist->Draw();
-    c->SaveAs(&("exp/"+name+".png")[0]);
-    hist->Write();
+    histo->Fit("f");
+    histo->Draw();
+    //c->SaveAs(&("exp/"+name+".png")[0]);
+    //histo->Write();
 }
     
 void create_histo(){
     // name of file | number of channels for the time difference | number of bin | range inf exp | range sup exp | norm exp | tau exp
     map <string, vector<double>> datasets ={
-                        //{"prova", {2, 0, 1, 1, 1}},
-                        //{"prova1", {2, 0, 1, 1000, 0.03}},
-                        {"5_5_weekend_4fe", {2, 500, 0, 10*pow(10,-6), 1000, 0.3}},
+                        {"4fe_above_up_run1", {500, 0, 10, 1000, 0.3, 1000, 0.3}},
+                        {"4fe_above_down_run1", {500, 0, 3, 1000, 0.3, 1000, 0.3}},
+                        {"4fe_above_up_run2", {500, 0, 10, 1000, 0.3, 1000, 0.3}},
+                        {"4fe_above_down_run2", {500, 0, 3, 1000, 0.3, 1000, 0.3}},
     };
-    vector<vector<double>> diff_time;  
+    vector<TTree*> trees;  
     vector<TH1D*> histos;
-    TFile *histo_file= new TFile(&("exp/histos_exp.root")[0], "RECREATE");
+    TH1D* histo;
+
+    TFile *tree_file= new TFile("tree.root", "READ");
+    //TFile *histo_file= new TFile("histo.root", "RECREATE");
     for (const auto &dataset : datasets){
         const auto name = dataset.first;
-        const auto numb = dataset.second[0];
-        const auto range = {dataset.second[1], dataset.second[2],dataset.second[3]};
-        const auto exp_param = {dataset.second[4], dataset.second[5]};
+        const auto ranges = {dataset.second[0], dataset.second[1], dataset.second[2]};
+        const auto exp_param = {dataset.second[3], dataset.second[4], dataset.second[5], dataset.second[6]};
 
-        diff_time = read_time(name, numb);
-        histos=fill_histo(diff_time, name, range);  
+        histo = fill_histo(tree_file, name, ranges);  
+
         for (int i=0; i<histos.size();i++){
-            fit_exp(histos[i], range, exp_param);
+            fit_exp(histo, ranges, exp_param);
         }
+        histos.push_back(histo);
     }
-    histo_file->Close();
-    getchar();
 
+    //histo_file->Close();
+    getchar();
 }
--- main_func.cpp
+++ main_func.cpp
@@ -3,100 +3,85 @@
 #include <vector>
 using namespace std;
 
+
 vector<vector<double>> read_time(string name, int numb){
     ifstream myfile;
-    myfile.open(name+".dat", ios::in | ios::out);  
-    vector<vector<double>> v (7);
-    vector<vector<double>> w (numb);    
+    myfile.open("data/"+name+".dat", ios::in | ios::out);  
+    double v1, v4;
+    vector<vector<double>> w (numb);
 
     if (myfile.is_open()){
         string tp;
+        double number;
         while(getline(myfile, tp)){ //read data from file object and put it into string.
+            number = stod(tp.substr(2, tp.length()-2));
             int ch_numb = stoi(tp.substr(0));
-            //cout <<ch_numb << tp.substr(8, tp.length()-8)<<endl;
-            v[ch_numb-1].push_back(stod(tp.substr(8, tp.length()-8))); 
-            if (ch_numb != 1){
-                w[ch_numb-2].push_back(v[ch_numb-1][v[ch_numb-1].size()-1]-v[0][v[0].size()-1]);
+            if (ch_numb==1) v1=number;
+            else if (ch_numb==4) v4=number;
+            else if (ch_numb >1 && ch_numb < 4){
+                w[ch_numb-2].push_back((number-v1)*pow(10, 6));
+
+                if (w[ch_numb-2][w[ch_numb-2].size()-1] >40 || w[ch_numb-2][w[ch_numb-2].size()-1] <0){
+                    cout <<w[ch_numb-2][w[ch_numb-2].size()-1]<<endl;
+                    cout << setprecision(12)<<v1 << endl;
+                }
+            }
+            if (numb==4){
+                if (ch_numb >4 && ch_numb < 7){
+                    w[ch_numb-3].push_back((number-v4)*pow(10, 6));
+                    //cout <<w[ch_numb-3][w[ch_numb-3].size()-1]<<endl;
+                    //cout << setprecision(12)<<v[0][v[0].size()-1] << endl;
+                }
             }
         }
         myfile.close(); //close the file object.
     }    
     return w;
-}
-string create_branches(int numb){
-    string branches="t1";
-    for (int i=1; i<numb; i++){
-        branches.append(":t"+to_string(i+1));
-    }
-    return branches;
-}
-void create_ntuple(string name, vector<vector<double>> diff_time, string branches){
-    TTree *data= new TTree(&(name)[0], &(name)[0]);
-    string branch_name;
-    //TBranch *b;
+} 
+
+void create_ttree(vector<string> materials, string run, vector<vector<double>> diff_time){
+    vector<string> positions = {"_above_up","_above_down","_below_up","_below_down"};
     for (int i=0; i<diff_time.size(); i++){
-        double t[diff_time[i].size()];
-        branch_name = "t"+to_string(i);
-        TBranch *b=data->Branch(&(branch_name)[0], t, "t[diff_time[i].size()]/D");
-        //TBranch *b=data->Branch(&(branch_name)[0], &t, &(branch_name+"/D")[0]);
+        string name=materials[i]+positions[i]+run;
+        TTree *data= new TTree(&(name)[0], &(name)[0]);
+        double t;
+        TBranch *b=data->Branch("t", &t,"t/D");
         for (int j=0; j<diff_time[i].size(); j++){
-            t[j]=diff_time[i][j];
-            //cout << t <<endl;
-            b->Fill();
+            t=diff_time[i][j];
+            data->Fill();
         }
-        data->SetEntries(1);
-        cout <<endl;
-        data->Scan();
-        //data->Print();
+        //data->Scan();
+        data->Write();
     }
-    data->Write();
+    //return data;
 }
+
 
 void main_func(){
 
-    map <string, vector<int>> datasets ={
-                        {"prova", {2}},
+    map <string, vector<string>> datasets ={
+                        {"4fe_run1", {"fe4", "fe4"}},
+                        {"4fe_run2", {"fe4", "fe4"}}
+
     };
     vector<vector<double>> diff_time;  
-    string branches;  
-    TFile *ntuple_file= new TFile(&("ntuples.root")[0], "RECREATE");
+    map <string, vector<TTree*>>  tree_map = {
+        {"fe", {}},
+        {"al", {}},
+    };    
+    TFile *tree_file= new TFile(&("tree.root")[0], "RECREATE");
     for (const auto &dataset : datasets){
         const auto name = dataset.first;
-        const auto numb = dataset.second[0];
+        const auto materials = dataset.second;
+        int numb = materials.size();
+        string run = name.substr(name.find("_run"));
+        cout << "Processing : " << name << endl;
         diff_time = read_time(name, numb);
-        //branches = create_branch(numb);
-        create_ntuple(name, diff_time, branches);
-
-    }
-    ntuple_file->Close();
-}
-
-    /*for (int i=0; i<2; i++){
-        for (int j=0; j<10; j++){
-            cout << w[i][j] <<endl;
-        }
+        create_ttree(materials, run, diff_time);
         cout << endl;
-    }*/
-
-
-
-/*
-
-    TH1D* h1= new TH1D("exp", "exp", 20000, 0, 1);
-
-    for (int i=0; i<1; i++){
-        TCanvas *c = new TCanvas("exp", "exp");
-        TH1D* h= new TH1D("exp", "exp", 200, 0, 0.03);        
-        for (int j=0; j<w[i].size(); j++){
-            h->Fill(w[i][j]);
-            h1->Fill(w[i][j]);
-        }
-        h->Draw();
-        c->SaveAs(&("exp_channel"+to_string(i+1)+".pdf")[0]);
     }
-    TCanvas *c1 = new TCanvas("exp1", "exp1");
-    h1->Draw();
-    c1->SaveAs(&("exp_tot.pdf")[0]);
-*/
 
 
+
+    tree_file->Close();
+}
